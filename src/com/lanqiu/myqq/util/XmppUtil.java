c4f6c28e75343319d97ecc87e3653e4f6db27457
Zhanglanqiu <mrlanqiuz.@163.com>
2016-12-07 17:43:30 +0800

    完善XmppUtil中...

--- src/com/lanqiu/myqq/util/XmppUtil.java
+++ src/com/lanqiu/myqq/util/XmppUtil.java
@@ -250,4 +250,172 @@ public class XmppUtil {
 
 		return entriesList;
 	}
+
+	/**
+	 * 返回所有用户信息
+	 * 
+	 * @param roster
+	 * @return
+	 */
+	public static List<RosterEntry> getAllEntries(Roster roster) {
+		// 创建一个存储RosterEntry的集合
+		List<RosterEntry> enList = new ArrayList<RosterEntry>();
+		Collection<RosterEntry> entries = roster.getEntries();
+		Iterator<RosterEntry> i = entries.iterator();
+		while (i.hasNext())
+			enList.add(i.next());
+		return enList;
+	}
+
+	/**
+	 * 创建一个组 这个方法很简单通过asmack一行代码就可以创建一个群组 前提是需要传入花名册和群组名称
+	 * 
+	 * @param roster
+	 * @param groupName
+	 * @return
+	 */
+	public static boolean addGroup(Roster roster, String groupName) {
+		try {
+			roster.createGroup(groupName);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	/**
+	 * 删除一个组
+	 * 
+	 * @param roster
+	 * @param groupName
+	 * @return
+	 */
+	public static boolean removeGroup(Roster roster, String groupName) {
+		return false;
+	}
+
+	/**
+	 * 添加一个好友无分组
+	 * 
+	 * @param roster
+	 * @param userName
+	 * @param name
+	 * @return
+	 */
+	public static boolean addUser(Roster roster, String userName, String name) {
+		try {
+			roster.createEntry(userName, name, null);
+			return true;
+		} catch (XMPPException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	/**
+	 * 添加一个好友到一个分组下
+	 * 
+	 * @param roster
+	 * @param userName
+	 * @param name
+	 * @param groupName
+	 * @return
+	 */
+	public static boolean addUsers(Roster roster, String userName, String name,
+			String groupName) {
+		try {
+			roster.createEntry(userName, name, new String[] { groupName });
+			return true;
+		} catch (XMPPException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	/**
+	 * 删除一个好友
+	 * 
+	 * @param roster
+	 * @param userJid
+	 * @return
+	 */
+	public static boolean removeUser(Roster roster, String userJid) {
+		// 通过用户Jid删除指定用户
+		try {
+			RosterEntry entry = roster.getEntry(userJid);
+			roster.removeEntry(entry);
+			return true;
+		} catch (XMPPException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	/**
+	 * 把一个好友添加到一个组中
+	 * 
+	 * @param userJid
+	 * @param groupName
+	 * @param connection
+	 */
+	public static void addUserToGroup(final String userJid,
+			final String groupName, final XMPPConnection connection) {
+		// 获取传入的组名
+		RosterGroup group = connection.getRoster().getGroup(groupName);
+		// 这个组已经存在就添加到这个组，不存在创建一个组获取添加的好友
+		RosterEntry entry = connection.getRoster().getEntry(userJid);
+		try {
+			if (group != null) {
+				if (entry != null) {
+					group.addEntry(entry);
+				} else {
+					// 创建一个新的组
+					RosterGroup newGroup = connection.getRoster().createGroup(
+							"我的好友");
+					if (entry != null) {
+						newGroup.addEntry(entry);
+					}
+				}
+			}
+		} catch (XMPPException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 把一个好友从组中删除
+	 * 
+	 * @param userJid
+	 * @param groupName
+	 * @param connection
+	 */
+	public static void removeUserFromGroup(final String userJid,
+			final String groupName, final XMPPConnection connection) {
+		// 获取组
+		RosterGroup group = connection.getRoster().getGroup(groupName);
+		try {
+			if (group != null) {
+				// 获取好友
+				RosterEntry entry = connection.getRoster().getEntry(userJid);
+				if (entry != null) 
+					group.removeEntry(entry);
+			}
+		} catch (XMPPException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 修改签名
+	 * @param connection
+	 * @param code
+	 * @param content
+	 */
+	public static void changeSign(XMPPConnection connection,int code , String content){  
+		
+	}
+
 }
